da51ce928a5ebdd9a0315ca6d6a539eb01b161d6
Jiyang Wu <316400769@qq.com>
2025-02-16 21:58:00 +0800

    version 0.0.2

--- init.lua
+++ init.lua
@@ -3,5 +3,11 @@ require("plugins.plugins-setup")
 require("core.options")
 require("core.keymaps")
 
+-- Plugins
 require("plugins.lualine")
 require("plugins.nvim-tree")
+require("plugins.treesitter")
+require("plugins.lsp.mason")
+require("plugins.lsp.lspconfig")
+require("plugins.lsp.trouble")
+require("plugins.autopairs")
