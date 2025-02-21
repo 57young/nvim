4440e26aa9138a98cfffc626bc330af1259cc993
Jiyang Wu <316400769@qq.com>
2025-02-21 16:49:27 +0800

    LazyVim version 0.0.1

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+return {
+  -- add gruvbox
+  { "sainnhe/everforest" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
